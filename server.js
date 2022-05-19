44162f12543b671e5a15dc2b2b73c08413746428
farihulrouf <farihulrouf@gmail.com>
2022-05-19 09:28:17 +0700

    owner

--- server.js
+++ server.js
@@ -29,9 +29,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const productRouters = require("./routes/product")
 const userRouters = require("./routes/user")
 const categoryRouters = require("./routes/category")
+const ownerRoutes = require("./routes/owner");
+
 app.use("/api", productRouters)
 app.use("/api", userRouters)
 app.use("/api", categoryRouters)
+app.use("/api", ownerRoutes);
 
 
 app.listen(3002, err => {
